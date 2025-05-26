73237ddeca481c29e2462468019c1a23acff560c
Vladislav Chervyakov <chervyakovve@mail.ru>
2025-05-26 16:13:22 +0700

    feat: simple migration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,25 @@ import { TrackModule } from './routes/track/track.module';
 import { ArtistModule } from './routes/artist/artist.module';
 import { AlbumModule } from './routes/album/album.module';
 import { FavsModule } from './routes/favs/favs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, TrackModule, ArtistModule, AlbumModule, FavsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'host',
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USER || 'user',
+      password: process.env.POSTGRES_PASSWORD || '123',
+      database: process.env.POSTGRES_DB || 'postgres',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+    UserModule,
+    TrackModule,
+    ArtistModule,
+    AlbumModule,
+    FavsModule,
+  ],
 })
 export class AppModule {}
